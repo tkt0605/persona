8cf600ce0e0f8225bec01e60a62fef93a5abd034
tkt0605 <takato.komada2005@outlook.jp>
2025-12-06 11:55:27 +0900

    first commit

---
+++ frontend/app/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+export default function IndexPage(){
+  return(
+    <header className="flex items-center justify-between">
+      <div className="">
+        <button className="">Persona</button>
+      </div>
+      <div className="">
+        <button className=""></button>
+      </div>
+    </header>
+  );
+}
